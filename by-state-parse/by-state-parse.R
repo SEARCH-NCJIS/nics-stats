8b06904c197a8e037af0f69051deed3e1870cd94
Scott Came <scottcame10@gmail.com>
2019-09-03 17:42:37 -0700

    Update for 2016-2017 data

--- by-state-parse/by-state-parse.R
+++ by-state-parse/by-state-parse.R
@@ -2,20 +2,26 @@ library(tidyverse)
 library(tabulizer)
 library(openxlsx)
 
-# download the by-state file and then reference the file in the following line; it will load a mini-ui via shiny in which you'll need to highlight the table on p. 2
+# download the by-state file and then reference the file in the following line; it will load a mini-ui via shiny in which you'll need to highlight the table on whatever page it occurs
 
-nics <- extract_areas('~/Downloads/active-records-nics-indices-by-state-2018.pdf', 2)
+nics <- list()
+
+nics$df2016 <- extract_areas('raw-pdfs/2016.pdf', 4) %>% .[[1]]
+nics$df2017 <- extract_areas('raw-pdfs/2017.pdf', 4) %>% .[[1]]
+nics$df2018 <- extract_areas('raw-pdfs/2018.pdf', 2) %>% .[[1]]
 
 # (note: tried to capture the areas with locate_areas, then programatically do extract_areas, and tabulizer didn't like it, for whatever reason :shrug: )
 
 # then run this to create a tibble from it:
 
-nics <- nics[[1]] %>%
-  as_tibble() %>%
-  set_names(c('State', 'Felony', 'Indictment','FugitiveFromJustice','ControlledSubstance','AdjudicatedMentalHealth','Alien','DishonorableDischarge','RenouncedCitizenship','DVPO','MCDV','StateProhibitor','Total')) %>%
-  mutate_all(function(v) gsub(x=v, pattern=',', replacement='')) %>%
-  mutate_at(vars(-State), as.integer)
+nicsClean <- nics %>%
+  map(function(tdf) {
+    tdf %>% as_tibble() %>%
+      set_names(c('State', 'Felony', 'Indictment','FugitiveFromJustice','ControlledSubstance','AdjudicatedMentalHealth','Alien','DishonorableDischarge','RenouncedCitizenship','DVPO','MCDV','StateProhibitor','Total')) %>%
+      mutate_all(function(v) gsub(x=v, pattern=',', replacement='')) %>%
+      mutate_at(vars(-State), as.integer)
+  })
 
 # and then this, if you want to write it into an Excel spreadsheet:
 
-write.xlsx(nics, '/tmp/nics.xlsx')
+write.xlsx(nicsClean, 'output/nics.xlsx')
